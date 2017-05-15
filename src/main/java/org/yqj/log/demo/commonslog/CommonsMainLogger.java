1161daf93339b48b88c01b72179d7fafc0092a0e
yaoqijun <yaoqijunmail@gmail.com>
2017-05-16 00:14:46 +0800

    添加日志文件配置方式

---
+++ src/main/java/org/yqj/log/demo/commonslog/CommonsMainLogger.java
@@ -0,0 +1,24 @@
+package org.yqj.log.demo.commonslog;
+
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by yaoqijun on 2017/5/15.
+ */
+public class CommonsMainLogger {
+
+//    private static final Log  logger = LogFactory.getLog(CommonsMainLogger.class);
+//
+//    public static void main(String[] args) {
+//        if(logger.isTraceEnabled()){
+//            logger.trace("commons-logging-jcl trace message");
+//        }
+//        if(logger.isDebugEnabled()){
+//            logger.debug("commons-logging-jcl debug message");
+//        }
+//        if(logger.isInfoEnabled()){
+//            logger.info("commons-logging-jcl info message");
+//        }
+//    }
+}
--- src/main/java/org/yqj/log/demo/logback/LogbackTest.java
+++ src/main/java/org/yqj/log/demo/logback/LogbackTest.java
@@ -1,30 +1,30 @@
 package org.yqj.log.demo.logback;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import ch.qos.logback.classic.LoggerContext;
+//import ch.qos.logback.core.util.StatusPrinter;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 /**
  * Created by yaoqijun on 2017-05-15.
  */
 public class LogbackTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(LogbackTest.class);
-
-    public static void main(String[] args){
-        if(logger.isDebugEnabled()){
-            logger.debug("slf4j-logback debug message");
-        }
-        if(logger.isInfoEnabled()){
-            logger.info("slf4j-logback info message");
-        }
-        if(logger.isTraceEnabled()){
-            logger.trace("slf4j-logback trace message");
-        }
-
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        StatusPrinter.print(lc);
-    }
+//    private static final Logger logger = LoggerFactory.getLogger(LogbackTest.class);
+//
+//    public static void main(String[] args){
+//        if(logger.isDebugEnabled()){
+//            logger.debug("slf4j-logback debug message");
+//        }
+//        if(logger.isInfoEnabled()){
+//            logger.info("slf4j-logback info message");
+//        }
+//        if(logger.isTraceEnabled()){
+//            logger.trace("slf4j-logback trace message");
+//        }
+//
+//        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+//        StatusPrinter.print(lc);
+//    }
 
 }
